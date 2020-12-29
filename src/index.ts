e43e02d4e250cef405e52ad4d9c4614e31919ef8
alan <alandias01@yahoo.com>
2020-12-28 22:58:11 -0500

    eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-console.log("Node works");
+console.log('Node works');
 
-app.get("/", (req, res) => {
-  res.status(200).json({ success: "true" });
-
-  console.log("route hit");
+app.get('/', (req, res) => {
+  res.status(200).json({ success: 'true' });
+  console.log('route hit');
 });
 
 app.listen(3000, () => {
-  console.log("Listening");
+  console.log('Listening');
 });
